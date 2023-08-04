7e2e90256140aef0e10134cb73dc8f12f88e534d
Marcos Candeia <marrcooos@gmail.com>
2023-08-04 18:52:26 -0300

    Update deco version
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- deps.ts
+++ deps.ts
@@ -1,2 +1,1 @@
-export * from "https://denopkg.com/deco-cx/deco@65450405b501974793f25ed51d60a932b6eaed85/types.ts";
-export * from "https://denopkg.com/deco-cx/deco@65450405b501974793f25ed51d60a932b6eaed85/blocks/app.ts";
+export * from "https://denopkg.com/deco-cx/deco@1.23.0/mod.ts";
